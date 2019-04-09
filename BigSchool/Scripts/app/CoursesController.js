08955acbe0ae019a0efddb240f6e59c1ef8ac68b
Gia Khang <Gia Khang@DESKTOP-7SLU0VU>
2019-04-09 07:43:14 +0700

    Add project files.

---
+++ BigSchool/Scripts/app/CoursesController.js
@@ -0,0 +1,35 @@
+﻿var CoursesController = function () {
+    var button;
+    var init = function () {
+        $(".js-toggle-attendance").click(toggleAttedance);
+    };
+    var toggleAttendance = function (e) {
+        button = $(e.target);
+        if (button.hasClass("btn-default")) {
+            createAttendance();
+        }
+        else { deleteAttendance(); }
+
+    };
+    var createAttendance = function () {
+        $.post("/api/attendances/", { courseId: button.attr("data-course-id") })
+                .done(done).fail(fail);
+    };
+    var deleteAttendance = function () {
+        $.ajax({
+            url: "/api/attendances/" + button.attr("data-course-id"),
+            method: "DELETE"
+        })
+            .done(done).fail(fail);
+    };
+    var done = function () {
+        var text = (button.text() == "Going") ? "Going?" : "Going";
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+    var fail = function () {
+        alert("Something Failed");
+    };
+    return {
+        init: init
+    }
+}();
